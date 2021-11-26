16ac34a7e7310de2e1c0211478bb3cf39649951b
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-11-26 11:49:02 +0100

    remove shit

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -1,11 +1,5 @@
-import express, { json as parseJsonBody, Request } from 'express';
-import { createJwt, verifyJwtToken } from './jwtUtils';
-// import passport from 'passport';
-// import {} from 'passport-local';
-// import { ExtractJwt, StrategyOptions } from 'passport-jwt';
-// import jwt from 'jsonwebtoken';
-// import passport from 'passport';
-// import initPassport, {JWT_SECRET, JWT_AUDIENCE, JWT_ISSUER} from './jwtUtils';
+import express, { json as parseJsonBody } from 'express';
+import { createJwt } from './jwtUtils';
 
 
 const app = express();
@@ -14,11 +8,3 @@ app.use(parseJsonBody());
 
 const token = createJwt({gunnar: 'oledal', isccol: true, likes: ['food', 'skateboarding']}, 6000, 'asdfasdf');
 console.log('token:', token);
-
-try {
-  const decoded = verifyJwtToken(token);
-  console.log('decoded: ', decoded);
-} catch(e){
-  console.warn(e);
-
-}

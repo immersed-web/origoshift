b9abf5d866544ab31888afc8419e771203d0fd44
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-12-10 11:55:40 +0100

    Try to make aliases work in tests

--- backend/mediaserver/jest.config.js
+++ backend/mediaserver/jest.config.js
@@ -1,6 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+
+//Handle path aliases when testing
+import {pathsToModuleMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 module.exports = {
   roots: ['<rootDir>/src'],
+  moduleNameMapper: pathsToModuleMapper(compilerOptions.paths),
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'

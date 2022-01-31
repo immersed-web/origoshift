a336c7e89798a1aa08840353333f6cbafe7472e5
Gunnar <gunnar.oledal@gmail.com>
2022-01-31 11:49:45 +0000

    client page

--- app/src/pages/Client.vue
+++ app/src/pages/Client.vue
@@ -1,6 +1,46 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <h2 class="text-center">
+      Välkommen! Vilket event vill du besöka?
+    </h2>
+    <div class="row justify-around no-wrap">
+      <q-card class="q-ma-md col-6">
+        <h3 class="text-center">
+          Publika event
+        </h3>
+        <q-list>
+          <q-item
+            clickable
+            v-ripple
+            v-for="index in 8"
+            :key="index"
+          >
+            Cool event {{ index }}
+          </q-item>
+        </q-list>
+      </q-card>
+      <q-card
+        id="custom-event-container"
+        class="q-ma-md q-pa-lg col-6"
+      >
+        <h3 class="text-center">
+          Join a private event
+        </h3>
+        <div class="row q-gutter-md justify-between">
+          <q-input
+            outlined
+            dense
+            v-model="undefiend"
+            class="col-grow"
+          />
+          <q-btn
+            label="join"
+            color="primary"
+            icon-right="arrow_right"
+          />
+        </div>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
--- app/src/router/routes.ts
+++ app/src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/EmptyLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/client', component: () => import('pages/Client.vue') },
+    ],
   },
 
   // Always leave this as last one,

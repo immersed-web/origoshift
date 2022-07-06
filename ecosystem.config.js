a6b53553e06f737eecf0512d7440c26f5ccbbaa4
Gunnar <gunnar.oledal@gmail.com>
2022-07-06 10:21:45 +0000

    make it possible to run pm2 in dev mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,61 +3,62 @@ const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 
-  // let envVars;
-  // if (ctx.dev) {
-  //   envVars = dotenv.config({ path: '../.env.dev' }).parsed;
-  // } else {
-  //   envVars = dotenv.config({ path: '../.env' }).parsed;
-  // }
-  // console.log('envVars in quasar config:', envVars);
-  const envString = fs.readFileSync(path.join(__dirname, '.env'));
-  const envVars = dotenv.parse(envString);
-  // console.log('envVars in pm2 config:', envVars);
-  const authEnvs = _.pick(envVars, [
-    'DEVELOPMENT',
-    // 'AUTH_URL',
-    // 'AUTH_PORT',
-    // 'AUTH_PATH',
-    'DATABASE_URL',
-    'JWT_SECRET',
-    'JWT_ISSUER',
-    'JWT_AUDIENCE',
-    'SESSION_KEY',
-  ]);
-  // console.log('authenvs:', authEnvs);
-  // const frontendEnvs = _.pick(envVars, [
-  //   // 'SERVER_URL',
-  //   'AUTH_URL',
-  //   'AUTH_PORT',
-  //   'AUTH_PATH',
-  //   'MEDIASOUP_URL',
-  //   'MEDIASOUP_PORT',
-  //   'MEDIASOUP_PATH',
-  // ])
+let envString = fs.readFileSync(path.join(__dirname, '.env'));
+if(process.env.DEVELOPMENT){
+  envString = fs.readFileSync(path.join(__dirname, '.env.dev'));
+}
+const envVars = dotenv.parse(envString);
+const authEnvs = _.pick(envVars, [
+  'DEVELOPMENT',
+  'DATABASE_URL',
+  'JWT_SECRET',
+  'JWT_ISSUER',
+  'JWT_AUDIENCE',
+  'SESSION_KEY',
+]);
+
+const scripts = {
+  frontend: 'quasar serve ./dist/spa --history --port=8080',
+  auth: "yarn start",
+  mediaserver: 'yarn start',
+  caddy: 'caddy run'
+}
+
+if(envVars.DEVELOPMENT){
+  console.log('using dev scripts in ecosystem file!');
+  scripts.frontend = 'yarn dev';
+  scripts.auth = 'yarn dev';
+  scripts.mediaserver = 'yarn dev';
+}
+
 module.exports = {
   apps : [
     {
       name   : "frontend",
-      script : "quasar serve ./dist/spa --history --port=8080",
+      // script : "quasar serve ./dist/spa --history --port=8080",
       // script : "yarn dev",
+      script: scripts.frontend,
       cwd    : "./app/",
       // env    : frontendEnvs,
     },
     {
       name   : "auth",
-      script : "yarn start",
+      // script : "yarn start",
+      script: scripts.auth,
       cwd    : "./backend/auth/",
       env: authEnvs
     },
     {
       name   : "mediaserver",
-      script : "yarn start",
+      // script : "yarn start",
+      script: scripts.mediaserver,
       cwd    : "./backend/mediaserver/",
       env: envVars
     },
     {
       name: 'caddy',
-      script: "caddy run",
+      // script: "caddy run",
+      script: scripts.caddy,
       cwd: "./",
       env: {
         SERVER_URL: envVars.SERVER_URL

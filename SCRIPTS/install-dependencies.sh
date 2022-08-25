2fe7dcf431be6943a0296eef804719819a951782
Gunnar <gunnar.oledal@gmail.com>
2022-08-25 11:10:06 +0000

    Trying to handle some install issues

--- SCRIPTS/install-dependencies.sh
+++ SCRIPTS/install-dependencies.sh
@@ -3,11 +3,11 @@
 # this din't work on ICE VM running Ubuntu
 # if (( $EUID != 0 )); then
 
-if [[ $(id -u) -ne 0 ]] ; then
-    echo "Please run as root"
-    echo 'This script runs a bunch of stuff that requires privileges in order to configure the environment'
-    exit
-fi
+# if [[ $(id -u) -ne 0 ]] ; then
+#     echo "Please run as root"
+#     echo 'This script runs a bunch of stuff that requires privileges in order to configure the environment'
+#     exit
+# fi
 
 # Function to display commands
 stage() { echo ' '; echo ' '; echo '========================================='; printf "| $@ \n" ; echo '========================================='; echo '  ';}
@@ -26,7 +26,7 @@ stage 'Welcome to the install script!
 read -p "Press ENTER to continue. Press ctrl-c to cancel."
 # set -x
 say 'Updating package register'
-exe apt-get update
+exe sudo apt-get update
 
 stage 'Node, yarn and other global javascript dependencies'
 ######### Node and yarn
@@ -45,10 +45,13 @@ say 'Verify node is installed'
 exe node --version
 
 say 'Install yarn'
-exe npm install -G yarn
+exe npm install --global yarn
 
 say 'Verify yarn is installed'
 exe yarn --version
+
+say "Install dotenv-cli so we can inject env files when running npm scripts"
+exe yarn global add dotenv-cli
 
 say 'Install pm2'
 exe yarn global add pm2
@@ -68,22 +71,22 @@ exe quasar --version
 ######### Mediasoup dependencies
 stage 'Mediasoup dependencies'
 say 'Install python and PIP'
-exe apt install python3 python3-pip
+exe sudo apt-get --yes install python3 python3-pip
 say 'Check python is callable'
 exe python3 --version
 
 say 'Install make and gcc/g++'
-exe apt install build-essential
+exe sudo apt-get --yes install build-essential
 
 ##### CADDY
 stage 'CADDY Server'
 say 'Get caddy dep package'
-exe apt install -y debian-keyring debian-archive-keyring apt-transport-https
+exe sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
 exe curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor --yes -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 # Dont use this next line with the exe function. it fucks up apt
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 say 'update index after fetching pack'
-exe apt update
+exe sudo apt-get update
 say 'actually install caddy'
 exe apt install caddy
 

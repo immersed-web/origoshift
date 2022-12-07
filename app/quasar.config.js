a2daa7cb4a9b90be0dc8bb2167b2dca345851a96
Gunnar <gunnar.oledal@ri.se>
2022-12-07 10:08:56 +0000

    Adjust ports to align with vite's default devserver port. Also enable vite HMR through the reverse proxy.

--- app/quasar.config.js
+++ app/quasar.config.js
@@ -108,8 +108,11 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
+      hmr: {
+        clientPort: 443
+      },
       // https: true
-      open: true, // opens browser window automatically
+      open: false, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework

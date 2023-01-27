53d401833f6a1fa683e1d1d620925448097aba59
Gunnar Oledal <gunnar.oledal@gmail.com>
2023-01-27 10:40:07 +0100

    project tweaking

--- packages/shared-modules/index.ts
+++ packages/shared-modules/index.ts
@@ -1,1 +1,4 @@
-// export * as jwtUtils from './jwtUtils';
+// TODO: Find a cleaner module/package.json setup so we get autocomplete when importing subdirectories from the shared packages
+// This should for example be autocompleted:
+// import { } from 'shared-modules/...'
+// above should offer 'jwtUtils', 'authUtils' and 'utilFns' in editor

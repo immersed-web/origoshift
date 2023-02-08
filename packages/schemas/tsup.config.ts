e27e999e4e5fa9a4494d3bc7de042a1c8b285faf
Gunnar Oledal <gunnar.oledal@gmail.com>
2023-02-08 22:25:20 +0100

    project tweaks

--- packages/schemas/tsup.config.ts
+++ packages/schemas/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  clean: true,
+  // clean: true,
   dts: true,
   onSuccess: 'tsc --emitDeclarationOnly --declaration --declarationMap'
 })

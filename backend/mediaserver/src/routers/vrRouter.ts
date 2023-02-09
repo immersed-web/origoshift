f6d7b2f284fe12b8d4b00a217f81bc4b9e37d82e
Martin Törnros <martin.tornros@ri.se>
2023-02-09 16:11:08 +0100

    Clear clients with C key down

--- backend/mediaserver/src/routers/vrRouter.ts
+++ backend/mediaserver/src/routers/vrRouter.ts
@@ -13,13 +13,17 @@ const ee = new TypedEmitter<VREvents>();
 
 type ClientTransforms = Record<JwtUserData['uuid'], ClientTransform>;
 // const clientPositions = new Map<JwtUserData['uuid'], ClientPosition>();
-const clientTransforms: ClientTransforms = {};
+let clientTransforms: ClientTransforms = {};
 
 export const vrRouter = router({
   transforms: router({
     updateTransform: p.input(ClientTransformSchema).mutation(({input, ctx}) =>{
       clientTransforms[ctx.uuid] = input;
       ee.emit('transforms', clientTransforms, ctx.uuid);
+    }),
+    clearTransforms: p.mutation(({input, ctx}) =>{
+      clientTransforms = {}
+      ee.emit('transforms', clientTransforms, '');
     }),
     getClientTransforms: procedure.query(() => {
       return clientTransforms;

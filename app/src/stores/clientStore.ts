61579389f4b0ac0988e7b8d3f43b1b5567033618
Gunnar Oledal <gunnar.oledal@gmail.com>
2023-02-20 09:56:41 +0100

    exported utility type for getting router outputs/inputs

--- app/src/stores/clientStore.ts
+++ app/src/stores/clientStore.ts
@@ -1,15 +1,15 @@
-import { ref, computed } from 'vue';
+// import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
-import type { getClient } from '@/modules/trpcClient';
-import type { ClientTransform, ConnectionId } from 'schemas';
+import type { RouterOutputs } from '@/modules/trpcClient';
+// import type { ClientTransform, ConnectionId } from 'schemas';
 
 export const useClientStore = defineStore('client', {
   state: () => ({
     greeting: '',
     health: '',
     heartbeat: '',
-    venuesAll: [] as Awaited<ReturnType<ReturnType<typeof getClient>['venue']['listMyVenues']['query']>>,
-    venuesLoaded: {} as Awaited<ReturnType<ReturnType<typeof getClient>['venue']['listLoadedVenues']['query']>>,
+    venuesAll: [] as RouterOutputs['venue']['listMyVenues'],
+    venuesLoaded: {} as RouterOutputs['venue']['listLoadedVenues'],
     positionData: {}
   }),
   getters: {

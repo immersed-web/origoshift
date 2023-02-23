7051412049edf2236408709f0a8eaf48e01c36ea
Martin Törnros <martin.tornros@ri.se>
2023-02-23 09:38:15 +0100

    Installed Daisy UI

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -28,7 +28,8 @@ module.exports = {
       },
     },
   },
-  // plugins: [
-  //   require('flowbite/plugin'),
-  // ],
+  plugins: [
+    // require('flowbite/plugin'),
+    require('daisyui'),
+  ],
 };

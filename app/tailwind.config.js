6c080cf17d60052e35a21a2d981496d081ce7bbd
Gunnar Oledal <gunnar.oledal@gmail.com>
2023-02-27 09:19:01 +0100

    cleaning a bit

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,35 +1,12 @@
 /* eslint-env node */
-
-// const colors = require('tailwindcss/colors');
-// const indielayer = require('@indielayer/ui/tailwind.preset');
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // darkMode: 'class',
-  // presets: [indielayer()],
   content: [
     './index.html',
     './**/*.vue',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-    'node_modules/@indielayer/ui/**/*',
-    // 'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
-    // 'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       primary: colors.emerald,
-  //       secondary: colors.slate,
-  //       weird: colors.pink,
-  //       success: colors.green,
-  //       warning: colors.yellow,
-  //       error: colors.pink,
-  //     },
-  //   },
-  // },
   plugins: [
-    // require('flowbite/plugin'),
     require('daisyui'),
   ],
   daisyui: {

d571533e727a077ef7b859760847214dae0edcca
Gunnar Oledal <gunnar.oledal@gmail.com>
2023-03-07 12:07:02 +0100

    get user media is slowly getting in place 🎈🎈

--- app/src/views/camera/CameraView.vue
+++ app/src/views/camera/CameraView.vue
@@ -1,60 +1,139 @@
 <template>
+  <LoggedInHeader />
   <div
-    v-if="false"
-    class="text-9xl"
+    class="m-6 text-6xl"
   >
     Kamera-vy!
   </div>
-  <LoginBox
-    v-else
-    title="Logga in som sändare"
-    description="Logga in här för att skicka kameraström"
-    :error="error"
-    @submit="tryToLogin"
+  <div class="m-6 btn-group">
+    <button
+      class="btn btn-primary"
+      @click="getMe()"
+    >
+      Get me
+    </button>
+    <button
+      v-if="permissionState !== 'granted'"
+      @click="requestPermission"
+      class="btn"
+    >
+      Request camera access
+    </button>
+  </div>
+  <pre>
+    Permission state: {{ permissionState }}
+    Video info: {{ videoInfo }}
+  </pre>
+  <select
+    v-model="pickedVideoInput"
+    class="select select-primary"
+  >
+    <option
+      v-for="(device, key) in videoDevices"
+      :key="key"
+      :value="device"
+    >
+      {{ device.label }} ({{ device.deviceId }})
+    </option>
+  </select>
+  <select
+    v-model="pickedAudioInput"
+    class="select select-primary"
+  >
+    <option
+      v-for="(device, key) in audioDevices"
+      :key="key"
+      :value="device"
+    >
+      {{ device.label }} ({{ device.deviceId }})
+    </option>
+  </select>
+  <video
+    autoplay
+    ref="videoTag"
   />
-  <button
-    class="btn"
-    @click="authStore.logout"
-  >
-    Logga ut
-  </button>
-  <button
-    class="btn"
-    @click="getMe()"
-  >
-    Get me
-  </button>
 </template>
 
 <style>
 </style>
 
 <script setup lang="ts">
-import LoginBox from '@/components/LoginBox.vue';
+import LoggedInHeader from '@/components/user/LoggedInHeader.vue';
 import { getMe } from '@/modules/authClient';
-import { useAuthStore } from '@/stores/authStore';
-import { useConnectionStore } from '@/stores/connectionStore';
-import { onBeforeMount, ref } from 'vue';
-const authStore = useAuthStore();
-const connectionStore = useConnectionStore();
+import { computed, onMounted, ref, shallowRef, watch } from 'vue';
 
-const error = ref<Error>();
-onBeforeMount(async () => {
-  if(authStore.hasCookie){
-    await authStore.restoreFromSession();
-  } else {
-    authStore.autoGuest();
-  }
+const videoTag = ref<HTMLVideoElement>();
+
+const mediaDevices = ref<MediaDeviceInfo[]>();
+const videoDevices = computed(() => {
+  return mediaDevices.value?.filter(dev => dev.kind === 'videoinput');
+});
+const audioDevices = computed(() => {
+  return mediaDevices.value?.filter(dev => dev.kind === 'audioinput');
 });
 
-async function tryToLogin(username: string, password: string) {
-  try {
-    authStore.login(username, password);
-    // const state = await client.value.sender.getClientState.query();
-  } catch (e) {
-    if(e instanceof Error){
-      error.value = e;
-    }
-  }
+const pickedAudioInput = shallowRef<MediaDeviceInfo>();
+watch(pickedAudioInput, (pickedDevice) => startAudio(pickedDevice));
+
+const pickedVideoInput = shallowRef<MediaDeviceInfo>();
+watch(pickedVideoInput, (pickedDevice) => startVideo(pickedDevice));
+
+function startAudio(audioDevice: MediaDeviceInfo){
+  navigator.mediaDevices.getUserMedia({
+    audio: {
+      deviceId: audioDevice.deviceId,
+    },
+  });
 }
+const videoTrack = shallowRef<MediaStreamTrack>();
+const videoInfo = computed(() => {
+  if(!videoTrack.value) return undefined;
+  const {width, height, frameRate} = videoTrack.value.getSettings();
+  return {
+    width, height, frameRate,
+  };
+});
+async function startVideo(videoDevice: MediaDeviceInfo){
+  console.log('starting video!!');
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      deviceId: {
+        exact: videoDevice.deviceId,
+      },
+      width: {
+        ideal: 4000,
+      },
+      height: {
+        ideal: 2200,
+      },
+    },
+  });
+  console.log(stream);
+
+  videoTrack.value = await stream.getVideoTracks()[0];
+  videoTag.value!.srcObject = stream;
+  videoTag.value!.play();
+}
+
+const permissionState = ref();
+onMounted(async () => {
+  const perm = await navigator.permissions.query({name: 'camera'});
+  permissionState.value = perm.state;
+  perm.onchange = ev => permissionState.value = perm.state;
+  mediaDevices.value = await navigator.mediaDevices.enumerateDevices();
+
+  navigator.mediaDevices.addEventListener('devicechange', async () => {
+    mediaDevices.value = await navigator.mediaDevices.enumerateDevices();
+  });
+});
+
+async function requestPermission() {
+  const result = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  mediaDevices.value = await navigator.mediaDevices.enumerateDevices();
+  result.getTracks().forEach(trk => trk.stop());
+}
+
 </script>

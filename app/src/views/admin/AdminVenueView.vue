14c21b4a72cb1284598a154d03746ce54a42264c
Martin Törnros <martin.tornros@ri.se>
2023-04-05 11:24:16 +0200

    Stylish event steps 🪜

--- app/src/views/admin/AdminVenueView.vue
+++ app/src/views/admin/AdminVenueView.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex mb-4">
       <div class="flex-1">
-        <h1 class="">
+        <h1>
           {{ venueStore.currentVenue?.name }}
         </h1>
       </div>
@@ -15,6 +15,52 @@
           Ta bort event
         </button>
       </div>
+    </div>
+    <!-- <div class="flex">
+      <div>
+        <div class="flex items-center">
+          <span class="mr-2 material-icons">meeting_room</span>
+          {{ venueStore.currentVenue?.doorsOpeningTime?.toLocaleString() }}
+        </div>
+        <div class="flex items-center">
+          <span class="mr-2 material-icons">curtains</span>
+          {{ venueStore.currentVenue?.streamStartTime?.toLocaleString() }}
+        </div>
+      </div>
+    </div> -->
+    <div>
+      <ul class="steps w-full">
+        <li class="step step-info">
+          <div class="flex items-center text-sm">
+            <span class="mr-2 material-icons">meeting_room</span>
+            {{ venueStore.currentVenue?.doorsOpeningTime?.toLocaleString() }}
+          </div>
+          <span class="text-xs">
+            Dörrarna till eventet öppnas <i>automatiskt</i> den angivna tiden.<br>
+            Om eventet har en VR-lobby har besökaren möjlighet att gå in i denna.
+          </span>
+        </li>
+        <li class="step step-info">
+          <div class="flex items-center text-sm">
+            <span class="mr-2 material-icons">curtains</span>
+            {{ venueStore.currentVenue?.streamStartTime?.toLocaleString() }}
+          </div>
+          <span class="text-xs">
+            Eventets/360-sändningens <i>planerade</i> starttid, synlig för besökarna.<br>
+            Ni startar eventet <i>manuellt</i> när föreställningen startar.
+          </span>
+        </li>
+        <li class="step step-info">
+          <div class="flex items-center text-sm">
+            <span class="mr-2 material-icons">curtains_closed</span>
+            Manuellt avslut
+          </div>
+          <span class="text-xs">
+            Eventet/360-sändningen avslutas.<br>
+            Ni avslutar eventet <i>manuellt</i> när föreställningen är slut.
+          </span>
+        </li>
+      </ul>
     </div>
     <div class="divider" />
     <div class="flex items-stretch">

cb6d84de12bcae5cdea0a7fd76cf84390651c30f
Gunnar Oledal <gunnar.oledal@ri.se>
2023-05-01 13:57:43 +0000

    started on aframe portal

--- app/src/views/user/UserCameraView.vue
+++ app/src/views/user/UserCameraView.vue
@@ -12,6 +12,71 @@
   </div>
   <div v-else>
     <h1>Watching camera: {{ props.cameraId }}</h1>
+    <input
+      type="range"
+      max="360"
+      min="-360"
+      v-model="xRot"
+    >
+    <input
+      type="range"
+      max="360"
+      min="-360"
+      v-model="yRot"
+    >
+    <input
+      type="range"
+      max="360"
+      min="-360"
+      v-model="zRot"
+    >
+    <a-scene
+      class="w-screen h-screen"
+      embedded
+    >
+      <a-camera reverse-mouse-drag="true" />
+      <a-box
+        position="-1 0.5 -3"
+        rotation="0 45 0"
+        color="#4CC3D9"
+      />
+      <a-sphere
+        position="0 1.25 -5"
+        radius="1.25"
+        color="#EF2D5E"
+      />
+      <a-cylinder
+        position="1 0.75 -3"
+        radius="0.5"
+        height="1.5"
+        color="#FFC65D"
+      />
+      <a-plane
+        position="0 0 -4"
+        rotation="-90 0 0"
+        width="4"
+        height="4"
+        color="#7BC8A4"
+      />
+      <a-sky color="#ECECEC" />
+      <a-box
+        position="-1 0.5 -3"
+        rotation="0 45 0"
+        color="#4CC3D9"
+      />
+      <a-entity
+        v-for="portal in portalsWithStyles"
+        :key="portal.cameraId"
+        :rotation="`${xRot} ${yRot} ${zRot}`"
+      >
+        <!-- :rotation="`${90-180*parseFloat(portal.style.top)} 0 0`" -->
+        <a-sphere
+          position="10 0 0"
+          color="#ef2d5e"
+        />
+      </a-entity>
+      <a-videosphere />
+    </a-scene>
     <div class="flex">
       <div>
         <div class="relative">
@@ -25,6 +90,7 @@
           <video
             autoplay
             ref="videoTag"
+            id="main-video"
           />
         </div>
         <audio
@@ -37,8 +103,8 @@
         /> -->
       </div>
       <pre class="max-w-xl">
-      {{ soup.consumers }}
-    </pre>
+        {{ soup.consumers }}
+      </pre>
     </div>
   </div>
 </template>
@@ -51,6 +117,7 @@ import { computed, onBeforeMount, onBeforeUnmount, onMounted, reactive, ref, toR
 import { useVenueStore } from '@/stores/venueStore';
 import { useCameraStore } from '@/stores/cameraStore';
 import { useElementSize } from '@vueuse/core';
+import 'aframe';
 
 const props = defineProps<{
   venueId: VenueId,
@@ -62,6 +129,10 @@ const router = useRouter();
 const videoTag = ref<HTMLVideoElement>();
 const { width, height } = useElementSize(videoTag);
 const audioTag = ref<HTMLAudioElement>();
+
+const xRot = ref(0);
+const yRot = ref(0);
+const zRot = ref(0);
 
 const soup = useSoupStore();
 const venueStore = useVenueStore();
@@ -91,6 +162,9 @@ watch(() => camera.producers, async (updatedProducers) => {
     // receivedTracks.videoTrack = rcvdTracks.videoTrack;
     if(rcvdTracks.videoTrack && videoTag.value){
       videoTag.value.srcObject = new MediaStream([rcvdTracks.videoTrack]);
+      const vSphere = document.querySelector('a-videosphere');
+      // vSphere.setAttribute('srcObject', 'https://bitmovin.com/player-content/playhouse-vr/progressive.mp4');
+      vSphere.setAttribute('src', '#main-video');
     }
     if(rcvdTracks.audioTrack && audioTag.value){
       audioTag.value.srcObject = new MediaStream([rcvdTracks.audioTrack]);

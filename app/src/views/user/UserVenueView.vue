53873d14437cdcbb4aeeb9b0a8983ef3fd3e5a5b
Martin Törnros <martin.tornros@ri.se>
2023-05-22 16:10:17 +0200

    User venue UX

--- app/src/views/user/UserVenueView.vue
+++ app/src/views/user/UserVenueView.vue
@@ -1,38 +1,82 @@
 <template>
   <div>
+    <h1 class="text-5xl font-bold my-2">
+      {{ venueInfo?.name }}
+    </h1>
+
     <div v-if="!venueStore.currentVenue">
-      <h1 class="my-6">
-        Väntar på att eventet öppnar...
-      </h1>
       <div>
+        <div class="alert alert-warning">
+          <div>
+            <span class="material-icons">
+              warning
+            </span>
+            Eventet är inte igång.
+          </div>
+        </div>
+        <h2>Eventets hålltider</h2>
         <p v-if="venueInfo?.doorsOpeningTime">
-          Dörrarna öppnas: {{ venueInfo.doorsOpeningTime }}
+          <strong>Lobbyn öppnar:</strong> {{ venueInfo.doorsOpeningTime.toLocaleString() }}
+        </p>
+        <p v-if="venueInfo?.streamStartTime">
+          <strong>Sändningen startar:</strong> {{ venueInfo.streamStartTime.toLocaleString() }}
+        </p>
+
+        <p v-if="venueInfo?.doorsOpeningTime && !isPast(venueInfo.doorsOpeningTime)">
+          Behåll denna sida öppen för att automatiskt slussas in i eventet när det startar.
         </p>
       </div>
     </div>
     <div
       v-else
     >
-      <h1
-        class="text-5xl font-bold"
-      >
-        {{ venueStore.currentVenue.name }}
-      </h1>
       <div class="flex space-x-2">
-        <button
-          class="btn btn-primary"
-          @click="openLobby"
-        >
-          Gå in i VR-lobby
-        </button>
-        <button
-          v-for="camera in venueStore.currentVenue.cameras"
-          :key="camera.cameraId"
-          @click="router.push({name: 'userCamera', params: {venueId: props.venueId, cameraId: camera.cameraId}})"
-          class="btn btn-primary"
-        >
-          {{ camera.name }}
-        </button>
+        <div class="flex-1">
+          <div v-if="venueStore.currentVenue.vrSpace">
+            <h2>VR-lobby</h2>
+
+            <span
+              class="material-icons text-sm"
+              :class="venueStore.currentVenue?.doorsAreOpen ? 'text-green-500' : 'text-red-500'"
+            >circle</span>
+            <strong>Lobbyn är {{ venueStore.currentVenue?.doorsAreOpen ? 'öppen' : 'stängd' }}</strong>
+
+            <div v-if="venueStore.currentVenue.doorsAreOpen">
+              <p>Gå in i eventets VR-lobby och träffa andra besökare till detta event. Du kan använda ett VR-headset eller mus och tangentbord.</p>
+              <button
+                class="btn btn-primary"
+                @click="openLobby"
+              >
+                Gå in i VR-lobby
+              </button>
+            </div>
+            <div v-else-if="venueStore.currentVenue.doorsOpeningTime">
+              <strong>Lobbyn öppnar:</strong> {{ venueStore.currentVenue.doorsOpeningTime?.toLocaleString() }}
+            </div>
+          </div>
+        </div>
+        <div class="flex-1">
+          <span
+            class="material-icons text-sm"
+            :class="venueStore.currentVenue?.streamIsActive ? 'text-green-500' : 'text-red-500'"
+          >circle</span>
+          <strong>Sändningen är {{ venueStore.currentVenue?.streamIsActive ? 'igång' : 'ej igång' }}</strong>
+
+          <div v-if="venueStore.currentVenue.streamIsActive">
+            <p>360-sändning blahi blaha</p>
+            <button
+              v-for="camera in venueStore.currentVenue.cameras"
+              :key="camera.cameraId"
+              @click="router.push({name: 'userCamera', params: {venueId: props.venueId, cameraId: camera.cameraId}})"
+              class="btn btn-primary"
+            >
+              {{ camera.name }}
+            </button>
+          </div>
+          <div v-else-if="venueStore.currentVenue.streamStartTime">
+            <strong>Sändningen startar:</strong> {{ venueStore.currentVenue.streamStartTime?.toLocaleString() }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -46,6 +90,7 @@ import type { VenueId, VenueListInfo } from 'schemas';
 import { onMounted, shallowRef } from 'vue';
 import { useVenueStore } from '@/stores/venueStore';
 import { useIntervalFn } from '@vueuse/core';
+import { isPast } from 'date-fns';
 const connection = useConnectionStore();
 const venueStore = useVenueStore();
 

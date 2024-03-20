02d98c6633b07fa6347a09d6ea3c553e2006d1b2
Gunnar <gunnar.oledal@ri.se>
2024-03-20 08:25:01 +0000

    Link to restart from logout view

--- app/src/views/LogoutView.vue
+++ app/src/views/LogoutView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="h-screen grid place-items-center">
-        <div class=" card bg-base-100 p-12">
+        <div class="card gap-12 items-end bg-base-100 p-12">
           <h2>Utloggad 👍</h2>
+          <RouterLink
+            class="flex gap-1 items-center text-primary"
+            :to="{name: 'enter'}"
+          >
+            Starta om
+            <span class="material-icons">keyboard_arrow_right</span>
+          </RouterLink>
         </div>
   </div>
 </template>
